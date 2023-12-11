75733caeaf483a868febe58c9dd4e4bdf4284998
TurtleWolfe <JonPohlner@gmail.com>
2023-12-10 20:33:20 -0500

    backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Helmet} from "react-helmet";
 import { ChatBot_UI } from './components/chatbot_UI/chatbot_UI'; // Add this line to import your chatbot
 
 import './App.css';
@@ -7,6 +8,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+            {/* Include the Head component with your meta tags here */}
+            <Helmet>
+        <title>geoLARP</title>
+        <meta property='og:title' content='geoLARP' />
+<meta property='og:image' content='https://geolarp.com/images/geoLARP_logo.png' />
+<meta property='og:description' content='Immerse yourself in a geo-located live action role-playing game where your physical location shapes the adventure. Explore and interact in a realm where fantasy and reality merge.' />
+<meta property='og:url' content='https://geolarp.com' />
+<meta property='og:type' content='website' />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@geoLARP" />
+<meta name="twitter:title" content="geoLARP" />
+<meta name="twitter:description" content="Immerse yourself in a geo-located live action role-playing game where your physical location shapes the adventure. Explore and interact in a realm where fantasy and reality merge." />
+<meta name="twitter:image" content="https://geolarp.com/images/geoLARP_logo.png" />
+<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+<meta name="description" content="Immerse yourself in a geo-located live action role-playing game where your physical location shapes the adventure. Explore and interact in a realm where fantasy and reality merge." />
+
+
+        
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Immerse yourself in a geo-located live action role-playing game where your physical location shapes the adventure. Explore and interact in a realm where fantasy and reality merge, offering a unique gaming experience." />
+        {/* Add other meta tags as needed */}
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
